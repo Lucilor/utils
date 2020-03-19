1961628b4b2697e16a878877cfdfb5c27af3e3be
Lucilor <lucilor@me.com>
2020-03-19 11:42:52 +0800

    clean

--- lib/src/angle.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Angle {
-    value: number;
-    unit: "rad" | "deg";
-    constructor(value?: number, unit?: "rad" | "deg");
-    getRad(): number;
-    getDeg(): number;
-    constrain(): this;
-    set(value?: number, unit?: "rad" | "deg"): this;
-}
