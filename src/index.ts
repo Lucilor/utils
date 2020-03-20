7c018caf43a72d3b964917eab6b5c9b4ee25b644
Lucilor <lucilor@me.com>
2020-03-20 17:12:18 +0800

    storage.ts

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import {Arc} from "./arc";
 import {Angle} from "./angle";
 import {RGB2Index, index2RGB} from "./color";
 import {dataURLtoBlob, RSAEncrypt} from "./misc";
+import {SessionStorage, LocalStorage} from "./storage";
 
-export {Point, Line, Rectangle, Angle, Arc, dataURLtoBlob, RSAEncrypt, RGB2Index, index2RGB};
+export {Point, Line, Rectangle, Angle, Arc, dataURLtoBlob, RSAEncrypt, RGB2Index, index2RGB, SessionStorage, LocalStorage};
