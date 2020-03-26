eeee773204272bf8ee4175afe16fc2ec7b8e1a03
Lucilor <lucilor@me.com>
2020-03-26 17:08:09 +0800

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,21 @@ import {Line} from "./line";
 import {Rectangle} from "./rectangle";
 import {Arc} from "./arc";
 import {Angle} from "./angle";
-import {RGB2Index, index2RGB} from "./color";
+import {RGB2Index, index2RGB, getColorLightness} from "./color";
 import {dataURLtoBlob, RSAEncrypt} from "./misc";
 import {SessionStorage, LocalStorage} from "./storage";
 
-export {Point, Line, Rectangle, Angle, Arc, dataURLtoBlob, RSAEncrypt, RGB2Index, index2RGB, SessionStorage, LocalStorage};
+export {
+	Point,
+	Line,
+	Rectangle,
+	Angle,
+	Arc,
+	dataURLtoBlob,
+	RSAEncrypt,
+	RGB2Index,
+	index2RGB,
+	getColorLightness,
+	SessionStorage,
+	LocalStorage
+};
