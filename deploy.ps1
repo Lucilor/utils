f3f1329c4e164259b4bfa48844740c0913c8a7d4
Lucilor <lucilor@me.com>
2020-11-13 09:51:07 +0800

    🔧build(deps): update deps

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,6 @@
 copy-item "C:\Users\91197\Documents\ng-cad2\src\app\utils\*" ".\src" -Force -Recurse
+Write-Host -NoNewLine 'Press any key to continue...';
+$Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 yarn lib && yarn dist
+remove-item ".\dist\*.LICENSE.*"
 copy-item ".\dist\*" "C:\Softwares\XAMPP\htdocs\n\static\js\jichu" -Force -Recurse
