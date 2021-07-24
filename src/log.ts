c03e99e16c1e4e56080c23aca011ac99108993d5
Lucilor <lucilor@me.com>
2021-07-24 10:03:46 +0800

    ✨ feat(log): log & Timer

---
+++ src/log.ts
@@ -0,0 +1,59 @@
+import {keysOf, ObjectOf} from "./types";
+
+export const log = (msg: string, type?: string, styles: Partial<CSSStyleDeclaration> = {}) => {
+    if (typeof type === "string" && type) {
+        type = `[${type}] `;
+    } else {
+        type = "";
+    }
+    const div = document.createElement("div");
+    keysOf(styles).forEach((key: any) => (div.style[key] = styles[key] as any));
+    const cssText = div.style.cssText;
+    let msg2 = type + msg;
+    if (cssText) {
+        if (!msg2.includes("%c")) {
+            msg2 = "%c" + msg2;
+        }
+        console.log(msg2, cssText);
+    } else {
+        console.log(msg2);
+    }
+};
+
+export class Timer {
+    private _pool: ObjectOf<number> = {};
+    fractionDigits = 2;
+
+    get now() {
+        return performance.now();
+    }
+
+    private _getTimeString(name: string) {
+        const time = (this.now - this._pool[name]) / 1000;
+        return time.toFixed(this.fractionDigits) + "s";
+    }
+
+    log(name: string, content: string) {
+        if (content) {
+            content += ": ";
+        }
+        log(`${content}${this._getTimeString(name)}`, "Timer", {color: "deeppink"});
+        return this;
+    }
+
+    start(name: string, content?: string) {
+        this._pool[name] = this.now;
+        if (typeof content === "string") {
+            this.log(name, content);
+        }
+        return this;
+    }
+
+    end(name: string, content?: string) {
+        if (typeof content === "string") {
+            this.log(name, content);
+        }
+        delete this._pool[name];
+        return this;
+    }
+}
