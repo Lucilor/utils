950d83ca0548bd09476e1d1ec1a99901463dbe32
Lucilor <lucilor@me.com>
2021-09-28 17:17:09 +0800

    ✨ feat: list-random

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./color";
 export * from "./files";
 export * from "./geometry";
+export * from "./list-random";
 export * from "./log";
 export * from "./math";
 export * from "./misc";
