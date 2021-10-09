8a6792f8a6bdc89cf6ef4ccaeb87473abce425af
Lucilor <lucilor@me.com>
2021-10-09 10:42:58 +0800

    ✨ feat(math): calc expression

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,4 +1,20 @@
+import {Parser} from "expr-eval";
+import md5 from "md5";
+import {ObjectOf} from "../types";
+
 export const fibonacci = (n: number) => {
     const sqrt5 = Math.sqrt(5);
     return Math.round((((1 + sqrt5) / 2) ** n - ((1 - sqrt5) / 2) ** n) / sqrt5);
+};
+
+export const calc = (expression: string, vars: ObjectOf<number>) => {
+    Object.keys(vars).forEach((key) => {
+        if (key.match("[^\x00-\xff]")) {
+            const key2 = md5(key);
+            expression = expression.replaceAll(key, key2);
+            vars[key2] = vars[key];
+            delete vars[key];
+        }
+    });
+    return Parser.evaluate(expression, vars);
 };
