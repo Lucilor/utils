f78fe4019a68377040f4a77369fc10d56dba10fe
Lucilor <lucilor@me.com>
2021-12-01 16:33:33 +0800

    🎈 perf(math): calc => calculate, calculateForce

--- src/math/index.ts
+++ src/math/index.ts
@@ -7,7 +7,17 @@ export const fibonacci = (n: number) => {
     return Math.round((((1 + sqrt5) / 2) ** n - ((1 - sqrt5) / 2) ** n) / sqrt5);
 };
 
-export const calc = (expression: string, vars: ObjectOf<number>) => {
+export const calculate = (expression: string, vars: ObjectOf<number>) => {
+    const result = {value: 0, error: null as any};
+    try {
+        result.value = calculateForce(expression, vars);
+    } catch (error) {
+        result.error = error;
+    }
+    return result;
+};
+
+export const calculateForce = (expression: string, vars: ObjectOf<number>) => {
     Object.keys(vars).forEach((key) => {
         if (key.match("[^\x00-\xff]")) {
             const key2 = md5(key);
@@ -16,5 +26,11 @@ export const calc = (expression: string, vars: ObjectOf<number>) => {
             delete vars[key];
         }
     });
+    const result = {value: 0, error: null as any};
+    try {
+        result.value = Parser.evaluate(expression, vars);
+    } catch (error) {
+        result.error = error;
+    }
     return Parser.evaluate(expression, vars);
 };
