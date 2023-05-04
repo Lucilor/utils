c2e712344735cdb043c2a8b832533f17bdc836f6
lucilor <lucilor@me.com>
2023-05-04 08:52:09 +0800

    🦄 refactor(prettier): sort imports

--- src/object.ts
+++ src/object.ts
@@ -1,5 +1,5 @@
-import {ObjectOf} from "./types";
 import {cloneDeep, isEmpty, isEqual} from "lodash";
+import {ObjectOf} from "./types";
 
 export const isObject = (obj: any) => typeof obj === "object" && obj !== null && !Array.isArray(obj);
 
